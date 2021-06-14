c0636774b1cff2037b06bed6e08a80bb6504c0ef
lukaskuko9 <lukaskuko9@gmail.com>
2021-06-14 16:50:54 +0200

    Project startup (#1)
    * created wpf project
    
    * basic word showing functionality

---
+++ Hangman/Hangman/Models/LetterModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hangman
+{
+    public class LetterModel
+    {
+        private const char _hiddenChar = '*';
+        public char HiddenChar => _hiddenChar;
+
+        public char Letter { get; set; }
+        public bool IsShown { get; set; } = false;
+
+        public LetterModel(char letter)
+        {
+            this.Letter = letter;
+        }
+
+        public override string ToString()
+        {
+            return $"{ Letter}";
+        }
+    }
+}
---
+++ Hangman/Hangman/Models/WordModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Hangman
+{
+    public class WordModel : ObservableCollection<LetterModel>
+    {
+        private string wordStr;
+        public WordModel(string wordStr)
+        {
+            this.wordStr = wordStr;
+            SetNewWord(this.wordStr);
+        }
+
+        public void SetNewWord(string wordStr)
+        {
+            this.Clear();
+            this.wordStr = wordStr;
+
+            for (int i = 0; i < wordStr.Length; i++)
+                Add(new LetterModel(wordStr[i]));
+
+            this[3].IsShown = true;
+        }
+    }
+}
