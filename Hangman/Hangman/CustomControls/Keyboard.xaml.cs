546f575d6cdcf04d80510be3ff2cbb05b30b3e8c
lukaskuko9 <lukaskuko9@gmail.com>
2021-06-14 17:47:54 +0200

    Keyboard and it's functionality (#2)
    * created wpf project
    
    * basic word showing functionality
    
    * added keyboard and its functionality

---
+++ Hangman/Hangman/CustomControls/Keyboard.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hangman
+{
+    /// <summary>
+    /// Interaction logic for Keyboard.xaml
+    /// </summary>
+    public partial class Keyboard : UserControl
+    {
+        public delegate void LetterClicked(char letter);
+        public event LetterClicked OnLetterClick;
+
+        public static DependencyProperty LetterMinWidthProperty = DependencyProperty.Register(nameof(LetterMinWidth), typeof(int), typeof(LetterControl));
+        public int LetterMinWidth
+        {
+            get { return (int)GetValue(LetterMinWidthProperty); }
+            set { SetValue(LetterMinWidthProperty, value); }
+        }
+
+        public Keyboard()
+        {
+            InitializeComponent();
+        }
+
+        private void LetterControl_OnLetterClick(char letter)
+        {
+            OnLetterClick?.Invoke(letter);
+        }
+    }
+}
---
+++ Hangman/Hangman/CustomControls/LetterControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hangman
+{
+    /// <summary>
+    /// Interaction logic for LetterControl.xaml
+    /// </summary>
+    public partial class LetterControl : UserControl
+    {
+        public delegate void LetterClicked(char letter);
+        public event LetterClicked OnLetterClick;
+
+        public static DependencyProperty LetterProperty = DependencyProperty.Register(nameof(Letter), typeof(char), typeof(LetterControl));
+        public char Letter
+        {
+            get { return (char)GetValue(LetterProperty); }
+            set { SetValue(LetterProperty, value); }
+        }
+
+        public LetterControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OnLetterClick?.Invoke(Letter);
+        }
+    }
+}
